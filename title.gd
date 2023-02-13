5a010e3a0d28449ce14bf5f5460078bfb3b39bad
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-02-12 23:06:13 -0800

    Add card buttons to title screen

--- title.gd
+++ title.gd
@@ -1,16 +1,25 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+# app semver
+var version = "0.0.1"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#$"MarginContainer/MainMenuContainer/Version Label".text = "Version %s" % version
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_StartButtonCard_pressed():
+	OS.alert("Start Button Pressed", "_on_StartButtonCard_pressed()")
+
+
+func _on_OptionsButtonCard_pressed():
+	OS.alert("Options Button Pressed", "_on_OptionsButtonCard_pressed()")
