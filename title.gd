ec771ef6db7650f34699b413497f7388c3215f93
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-02-13 13:34:45 -0800

    Add SceneTransitionRect

--- title.gd
+++ title.gd
@@ -4,12 +4,12 @@ extends Node
 
 # app semver
 var version = "0.0.1"
+onready var _transition_rect := $SceneTransitionRect
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$"MarginContainer/MainMenuContainer/Version Label".text = "Version %s" % version
-	pass
+	$"MarginContainer/MainMenuContainer/Version Label".text = "Version %s" % version
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,7 +18,7 @@ func _ready():
 
 
 func _on_StartButtonCard_pressed():
-	OS.alert("Start Button Pressed", "_on_StartButtonCard_pressed()")
+	_transition_rect.transition_to("res://TwentyOne.tscn")
 
 
 func _on_OptionsButtonCard_pressed():
