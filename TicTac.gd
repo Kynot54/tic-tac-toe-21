6b017406c07249e2a04409fd71552340dcd0c72e
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-02-15 17:26:31 -0800

    Add tictactoe scene

--- TicTac.gd
+++ TicTac.gd
@@ -1,16 +1,100 @@
 extends Node
 
+# state vars
+enum TicTacToeState {
+	IN_PROGRESS,
+	PLAYER_1_WIN, # player
+	PLAYER_2_WIN, # cpu
+	TIE
+}
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+enum RoundState {
+	PLAYER_1_PICKING,
+	PLAYER_2_PICKING,
+	IDLE
+}
 
+export var game_state = TicTacToeState.IN_PROGRESS
+export var round_state = RoundState.IDLE
+var player_1_squares = []
+var player_2_squares = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var buttons = $MarginContainer/CenterContainer/TicTacToeGrid.get_children()
+	# Link all the buttons to the common Pressed handler
+	for i in buttons.size():
+		buttons[i].connect("pressed", self, "on_grid_button_pressed", [buttons[i], i])
+	
+	self.reset_grid()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func on_grid_button_pressed(button, position):	
+	if self.round_state == RoundState.PLAYER_1_PICKING:
+		button.text = "X"
+		player_1_squares.append(position)
+		
+		if self.check_for_win(player_1_squares):
+			$DEBUGChangePlayerButton.disabled = true
+			$DEBUGCurrentPlayerLabel.text = "Player 1 Win"
+			self.game_state = TicTacToeState.PLAYER_1_WIN
+			self.round_state = RoundState.IDLE
+	elif self.round_state == RoundState.PLAYER_2_PICKING:
+		button.text = "O"
+		player_2_squares.append(position)
+		
+		if self.check_for_win(player_2_squares):
+			$DEBUGChangePlayerButton.disabled = true
+			$DEBUGCurrentPlayerLabel.text = "Player 2 Win"
+			self.game_state = TicTacToeState.PLAYER_2_WIN
+			self.round_state = RoundState.IDLE
+		
+
+func check_for_win(squares):
+	var win_horizontal = [[0,1,2], [3,4,5], [6,7,8]]
+	var win_vertical = [[0, 3, 6], [1,6,7], [2,5,8]]
+	var win_diagonal = [[0,4,8], [2,4,6]]
+	
+	if squares.size() >= 3:
+		for win_sequence_list in [win_horizontal, win_vertical, win_diagonal]:
+			for sequence in win_sequence_list:
+				if squares.has(sequence[0]) and squares.has(sequence[1]) and squares.has(sequence[2]):
+					return true
+	
+	return false
+		
+func reset_grid():
+	var buttons = $MarginContainer/CenterContainer/TicTacToeGrid.get_children()
+	$DEBUGCurrentPlayerLabel.text = "Idle (No Player Turn)"
+	$DEBUGChangePlayerButton.disabled = false
+	
+	for i in buttons.size():
+		buttons[i].text = String(i)
+		
+	player_1_squares = []
+	player_2_squares = []
+	
+	self.game_state = TicTacToeState.IN_PROGRESS
+	self.round_state = RoundState.IDLE
+
+func _on_DEBUGChangePlayerButton_pressed():
+	match self.round_state:
+		RoundState.IDLE:
+			self.round_state = RoundState.PLAYER_1_PICKING
+			$DEBUGCurrentPlayerLabel.text = "Player 1 Turn"
+		RoundState.PLAYER_1_PICKING:
+			self.round_state = RoundState.PLAYER_2_PICKING
+			$DEBUGCurrentPlayerLabel.text = "Player 2 Turn"
+		RoundState.PLAYER_2_PICKING:
+			self.round_state = RoundState.PLAYER_1_PICKING
+			$DEBUGCurrentPlayerLabel.text = "Player 1 Turn"
+	
+	
+
+
+func _on_DEBUGResetButton_pressed():
+	self.reset_grid()
