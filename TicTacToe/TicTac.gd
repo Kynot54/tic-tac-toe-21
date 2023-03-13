30afa8c817de244749bb415dfc57ffc200298c56
Kyle Whynott <kyle.whynott@gmail.com>
2023-03-13 03:05:20 -0700

    21 Scene Working and Updates to Tic-Tac-Toe In-Progress

--- TicTacToe/TicTac.gd
+++ TicTacToe/TicTac.gd
@@ -2,37 +2,32 @@ extends Node
 
 onready var TicTacToeBoard = $MarginContainer/CenterContainer/TicTacToeGrid
 
-func ready():
+func _ready():
 	self.TicTacToeBoard.reset_grid()
-	
-func _on_ResetGridButton_pressed():
-	$MarginContainer/DebugItems/StatusLabel.text = "Idle"
-	self.TicTacToeBoard.reset_grid()
+	if Deck.player1_win == true:
+		player_move()
+	elif Deck.player2_win == true:
+		ai_move()
 
-func _on_ChangePlayerButton_pressed():
-	if self.TicTacToeBoard.game_state != self.TicTacToeBoard.TicTacToeState.IN_PROGRESS:
-		return
-	
+func player_move():
+#	if self.TicTacToeBoard.game_state != self.TicTacToeBoard.TicTacToeState.IN_PROGRESS:
+#		return
 	if self.TicTacToeBoard.round_state == self.TicTacToeBoard.RoundState.IDLE or self.TicTacToeBoard.round_state == self.TicTacToeBoard.RoundState.PLAYER_2_PICKING:
 		$MarginContainer/DebugItems/StatusLabel.text = "Player 1 Picking"
 		self.TicTacToeBoard.round_state = self.TicTacToeBoard.RoundState.PLAYER_1_PICKING
-	else:
+
+func ai_move():
+#	if self.TicTacToeBoard.game_state != self.TicTacToeBoard.TicTacToeState.IN_PROGRESS:
+#		return
+	if self.TicTacToeBoard.round_state == self.TicTacToeBoard.RoundState.IDLE or self.TicTacToeBoard.round_state == self.TicTacToeBoard.RoundState.PLAYER_1_PICKING:
 		$MarginContainer/DebugItems/StatusLabel.text = "Player 2 Picking"
 		self.TicTacToeBoard.round_state = self.TicTacToeBoard.RoundState.PLAYER_2_PICKING
 
-func _on_MakeCPUMoveButton_pressed():
-	self.TicTacToeBoard.cpu_pick_square()
-
 func _on_TicTacToeGrid_onPlayer_1_win():
 	$MarginContainer/DebugItems/StatusLabel.text = "Player 1 Wins!"
-
 
 func _on_TicTacToeGrid_onPlayer_2_win():
 	$MarginContainer/DebugItems/StatusLabel.text = "Player 2 Wins!"
 
 func _on_TicTacToeGrid_onTie():
 	$MarginContainer/DebugItems/StatusLabel.text = "Tie :("
-
-
-func _on_BackButton_pressed():
-	Transit.change_scene("res://Title/Main.tscn")
