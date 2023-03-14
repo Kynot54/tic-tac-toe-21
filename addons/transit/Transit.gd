f01e5cb1765d3b3fc35c87c41c7502a6762750c3
Kyle Whynott <kyle.whynott@gmail.com>
2023-03-13 17:50:28 -0700

    Change to Tic-Tac-Toe Script

--- addons/transit/Transit.gd
+++ addons/transit/Transit.gd
@@ -9,36 +9,36 @@ onready var _animator := $AnimationPlayer
 onready var _curtain := $CanvasLayer/ColorRect
 
 func set_color(color: Color):
-    color.a = _curtain.color.a
-    _curtain.color = color
+	color.a = _curtain.color.a
+	_curtain.color = color
 
 func change_scene(path: String, duration: float = DEFAULT_DURATION, delay: float = DEFAULT_DELAY):
-    if duration <= 0.0:
-        push_error("TRANSIT ERROR: change_scene duration must be > 0. Defaulting to %s" % DEFAULT_DURATION)
-        duration = DEFAULT_DURATION
+	if duration <= 0.0:
+		push_error("TRANSIT ERROR: change_scene duration must be > 0. Defaulting to %s" % DEFAULT_DURATION)
+		duration = DEFAULT_DURATION
 
-    if delay < 0.0:
-        push_error("TRANSIT ERROR: change_scene delay must be >= 0. Defaulting to %s" % DEFAULT_DELAY)
-        delay = DEFAULT_DELAY
+	if delay < 0.0:
+		push_error("TRANSIT ERROR: change_scene delay must be >= 0. Defaulting to %s" % DEFAULT_DELAY)
+		delay = DEFAULT_DELAY
 
-    # disable mouse interaction while fading out
-    _curtain.mouse_filter = MOUSE_FILTER_STOP
+	# disable mouse interaction while fading out
+	_curtain.mouse_filter = MOUSE_FILTER_STOP
 
-    if delay > 0:
-        yield(get_tree().create_timer(delay), "timeout")
+	if delay > 0:
+		yield(get_tree().create_timer(delay), "timeout")
 
-    _animator.playback_speed = 1.0 / duration
-    _animator.play("fade")
-    yield(_animator, "animation_finished")
+	_animator.playback_speed = 1.0 / duration
+	_animator.play("fade")
+	yield(_animator, "animation_finished")
 
-    var err := get_tree().change_scene(path)
-    if err:
-        push_error("TRANSIT ERROR: Failed to change scene to %s: %s" % [path, err])
+	var err := get_tree().change_scene(path)
+	if err:
+		push_error("TRANSIT ERROR: Failed to change scene to %s: %s" % [path, err])
 
-    # re-enable mouse interaction before fading back in
-    _curtain.mouse_filter = MOUSE_FILTER_IGNORE
+	# re-enable mouse interaction before fading back in
+	_curtain.mouse_filter = MOUSE_FILTER_IGNORE
 
-    _animator.play_backwards("fade")
-    yield(_animator, "animation_finished")
+	_animator.play_backwards("fade")
+	yield(_animator, "animation_finished")
 
-    emit_signal("scene_changed")
+	emit_signal("scene_changed")
