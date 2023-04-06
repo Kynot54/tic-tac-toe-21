da9c94f2a0bd4a529b3d7f2c8a07a5dbc1514bd0
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-04-05 17:02:01 -0700

    Update Music.gd
    Improve format and documentation

--- Globals/Music.gd
+++ Globals/Music.gd
@@ -5,84 +5,101 @@ extends Node
 ## 	scenes.
 class_name BGM_SE_Player
 
+## Type of button control, for choosing sound effects
 enum ButtonType {
 	TITLE_BUTTON,
 	TWENTYONE_BUTTON,
 	TICTACTOE_BUTTON
 }
 
-onready var bgm_player = $BGMPlayer
-onready var se_player = $SEPlayer
+# AudioStreamPlayers used to play BGM and SE
+onready var _bgm_player = $BGMPlayer
+onready var _se_player = $SEPlayer
 
-var title_button_click = preload("res://Assets/Audio/SE/click1.ogg")
-var twentyone_button_click = preload("res://Assets/Audio/SE/click2.ogg")
-var tictactoe_button_click = preload("res://Assets/Audio/SE/click3.ogg")
+# sound effect files
+var _title_button_click = preload("res://Assets/Audio/SE/click1.ogg")
+var _twentyone_button_click = preload("res://Assets/Audio/SE/click2.ogg")
+var _tictactoe_button_click = preload("res://Assets/Audio/SE/click3.ogg")
 
-# volumes are in decibels, use db2linear() to convert to a slider value
-var bgm_volume setget set_bgm_volume, get_bgm_volume
-var se_volume setget set_se_volume, get_se_volume
+## Volume of background music, in decibels
+## if using a linear control to set/get (ex. [Slider]), convert
+## the linear value to decibels with linear2db() or convert
+## the decibel value to a linear value with db2linear().
+var bgm_volume setget _set_bgm_volume, _get_bgm_volume
+
+## Volume of sound effects, in decibels
+## if using a linear control to set/get (ex. [Slider]), convert
+## the linear value to decibels with linear2db() or convert
+## the decibel value to a linear value with db2linear().
+var se_volume setget _set_se_volume, _get_se_volume
 
 # config file to persist settings
-var config = ConfigFile.new()
+var _config = ConfigFile.new()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	title_button_click.loop = false
-	twentyone_button_click.loop = false
-	tictactoe_button_click.loop = false
+	# sound effects shouldn't loop
+	_title_button_click.loop = false
+	_twentyone_button_click.loop = false
+	_tictactoe_button_click.loop = false
 	
 	# attempt to read from configfile
-	var err = config.load("user://config.cfg")
+	var err = _config.load("user://config.cfg")
 	
+	# if config file doesn't exist, init to default values
 	if err != OK:
-		config.set_value("audio", "bgm_volume_db", 0)
-		config.set_value("audio", "se_volume_db", 0)
-		config.save("user://config.cfg")
+		_config.set_value("audio", "bgm_volume_db", 0)
+		_config.set_value("audio", "se_volume_db", 0)
+		_config.save("user://config.cfg")
 	
-	set_bgm_volume(config.get_value("audio", "bgm_volume_db"))
-	set_se_volume(config.get_value("audio", "se_volume_db"))
+	# read values from config file
+	_set_bgm_volume(_config.get_value("audio", "bgm_volume_db"))
+	_set_se_volume(_config.get_value("audio", "se_volume_db"))
 
 func _exit_tree():
-	config.save("user://config.cfg")
+	# save the config file to disk
+	_config.save("user://config.cfg")
 
 ## Set bgm volume in decibels
 ## if using a slider to modify the volume, convert linear to
 ## decibels using linear2db()
-func set_bgm_volume(new_volume):
-	bgm_player.volume_db = new_volume
-	config.set_value("audio", "bgm_volume_db", new_volume)
+func _set_bgm_volume(new_volume):
+	_bgm_player.volume_db = new_volume
+	_config.set_value("audio", "bgm_volume_db", new_volume)
 
 ## Returns bgm volume in decibels
 ## if using this value to set a slider's value,
 ## first call db2linear() on the returned value
-func get_bgm_volume():
-	return bgm_player.volume_db
+func _get_bgm_volume():
+	return _bgm_player.volume_db
 
 ## Set bgm volume in decibels
 ## if using a slider to modify the volume, convert linear to
 ## decibels using linear2db()
-func set_se_volume(new_volume):
-	se_player.volume_db = new_volume
-	config.set_value("audio", "se_volume_db", new_volume)
+func _set_se_volume(new_volume):
+	_se_player.volume_db = new_volume
+	_config.set_value("audio", "se_volume_db", new_volume)
 
 ## Returns bgm volume in decibels
 ## if using this value to set a slider's value,
 ## first call db2linear() on the returned value
-func get_se_volume():
-	return se_player.volume_db
+func _get_se_volume():
+	return _se_player.volume_db
 
-func play_button_click(button_type):
+## Play a button click sound effect
+## Pass a value of ButtonType to play a certain sound effect,
+## or nothing to play a default sound.
+func play_button_click(button_type=null):
 	match button_type:
 		ButtonType.TITLE_BUTTON:
-			se_player.stream = title_button_click
-			se_player.play()
+			_se_player.stream = _title_button_click
+			_se_player.play()
 		ButtonType.TWENTYONE_BUTTON:
-			se_player.stream = twentyone_button_click
-			se_player.play()
+			_se_player.stream = _twentyone_button_click
+			_se_player.play()
 		ButtonType.TICTACTOE_BUTTON:
-			se_player.stream = tictactoe_button_click
-			se_player.play()
+			_se_player.stream = _tictactoe_button_click
+			_se_player.play()
 		_:
-			se_player.stream = title_button_click
-			se_player.play()
+			_se_player.stream = _title_button_click
+			_se_player.play()
 		
