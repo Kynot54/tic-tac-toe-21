704e1af1b9ae27cbd6cf672649a6ed844e18e45a
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-04-19 17:37:28 -0700

    Change logo, animation

--- Title/title.gd
+++ Title/title.gd
@@ -6,7 +6,18 @@ var version = "0.1.0"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	
 	$"MarginContainer/MainMenuContainer/Version Label".text = "Version %s" % version
+	
+	var logo = $MarginContainer/MainMenuContainer/PanelContainer/Logo
+	var logo_container = $MarginContainer/MainMenuContainer/PanelContainer
+	var logo_container_center = logo_container.get_global_transform().get_origin()
+	
+	logo_container_center.x += logo_container.rect_size.x/2
+	logo_container_center.y += logo_container.rect_size.y/2
+	
+	logo.rect_pivot_offset.x = logo_container_center.x
+	logo.rect_pivot_offset.y = logo_container_center.y
 
 func _on_StartButtonCard_pressed():
 	Music.play_button_click(Music.ButtonType.TITLE_BUTTON)
