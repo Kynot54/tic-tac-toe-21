a77cc8e91c63ff0c5225814fa0ef87b252619ee8
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-04-24 23:57:04 -0700

    Reformat TTT Scripts

--- TicTacToe/TicTac.gd
+++ TicTacToe/TicTac.gd
@@ -12,23 +12,29 @@ func _ready():
 	elif gVar.player2_win == true:
 		ai_move()
 
+
 func _on_ChangePlayerButton_pressed():
 	if Board.game_state != Board.TicTacToeGameState.IN_PROGRESS:
 		return
-	
-	if Board.round_state == Board.TicTacToeRoundState.IDLE or Board.round_state == Board.TicTacToeRoundState.PLAYER_2_PICKING:
+
+	if (
+		Board.round_state == Board.TicTacToeRoundState.IDLE
+		or Board.round_state == Board.TicTacToeRoundState.PLAYER_2_PICKING
+	):
 		$MarginContainer/DebugItems/StatusLabel.text = "Player 1 Picking"
 		Board.round_state = Board.TicTacToeRoundState.PLAYER_1_PICKING
 	else:
 		$MarginContainer/DebugItems/StatusLabel.text = "Player 2 Picking"
 		Board.round_state = Board.TicTacToeRoundState.PLAYER_2_PICKING
 
+
 func player_move():
 	Board.round_state = Board.TicTacToeRoundState.PLAYER_1_PICKING
 	$MarginContainer/DebugItems/StatusLabel.text = "Player 1 Picking"
-	
+
 	yield(self.TicTacToeBoard, "onSquareSelected")
 	yield(get_tree().create_timer(2), "timeout")
+
 
 func ai_move():
 	Board.round_state = Board.TicTacToeRoundState.PLAYER_2_PICKING
@@ -40,11 +46,14 @@ func ai_move():
 	#print("picking square %s" % cpu_move)
 	$MarginContainer/CenterContainer/TicTacToeGrid.select_square(cpu_move)
 
+
 func _on_TicTacToeGrid_onPlayer_1_win():
 	$MarginContainer/DebugItems/StatusLabel.text = "Player 1 Wins!"
 
+
 func _on_TicTacToeGrid_onPlayer_2_win():
 	$MarginContainer/DebugItems/StatusLabel.text = "Player 2 Wins!"
+
 
 func _on_TicTacToeGrid_onTie():
 	$MarginContainer/DebugItems/StatusLabel.text = "Tie :("
--- TicTacToe/TicTacToeGrid.gd
+++ TicTacToe/TicTacToeGrid.gd
@@ -19,36 +19,37 @@ var player_2_value = 10
 export(Texture) var x_image
 export(Texture) var o_image
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var buttons = $ButtonLayer/TicTacToeGrid.get_children()
-	
+
 	# Link all the buttons to the common Pressed handler
 	for i in buttons.size():
 		buttons[i].connect("pressed", self, "on_grid_button_pressed", [buttons[i], i])
-	
+
 	self.init_grid()
+
 
 # Marks a square with an X or an O, depending on self.round_state
 func select_square(position):
-
 	Music.play_button_click(Music.ButtonType.TICTACTOE_BUTTON)
-	
+
 	var buttons = $ButtonLayer/TicTacToeGrid.get_children()
 	var texture_squares = $TextureLayer/TextureGridContainer.get_children()
-	
+
 	match Board.round_state:
 		Board.TicTacToeRoundState.PLAYER_1_PICKING:
 			buttons[position].disabled = true
 			Board.turns += 1
-			
+
 			# Set the square to an X
 			texture_squares[position].set_texture(x_image)
 			texture_squares[position].expand = true
-			
+
 			# Add the x to the board value array
 			Board.board[position] = player_1_value
-			
+
 			if self.check_for_win(0):
 				self.emit_signal("onPlayer_1_win")
 				Board.game_state = Board.TicTacToeGameState.PLAYER_1_WIN
@@ -60,17 +61,16 @@ func select_square(position):
 				Board.round_state = Board.TicTacToeRoundState.IDLE
 				self.emit_signal("onSquareSelected")
 
-			
 		Board.TicTacToeRoundState.PLAYER_2_PICKING:
 			# Disable the square's button
 			buttons[position].disabled = true
 			Board.turns += 1
-			
+
 			texture_squares[position].set_texture(o_image)
 			texture_squares[position].expand = true
-			
+
 			Board.board[position] = player_2_value
-			
+
 			if self.check_for_win(1):
 				self.emit_signal("onPlayer_2_win")
 				Board.game_state = Board.TicTacToeGameState.PLAYER_2_WIN
@@ -80,7 +80,7 @@ func select_square(position):
 			else:
 				Board.round_state = Board.TicTacToeRoundState.IDLE
 				self.emit_signal("onSquareSelected")
-	
+
 	if Board.turns == 9:
 		self.emit_signal("onTie")
 		Board.game_state = Board.TicTacToeGameState.TIE
@@ -88,50 +88,59 @@ func select_square(position):
 		Board.return_to = "res://Title/Main.tscn"
 		Transit.change_scene(Board.return_to)
 
-func on_grid_button_pressed(_button, position):	
+
+func on_grid_button_pressed(_button, position):
 	self.select_square(position)
-	
+
+
 func cpu_pick_square() -> int:
-	var row_horizontal = [[0,1,2], [3,4,5], [6,7,8]]
-	var row_vertical = [[0, 3, 6], [1,4,7], [2,5,8]]
-	var row_diagonal = [[0,4,8], [2,4,6]]
-	
+	var row_horizontal = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+	var row_vertical = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+	var row_diagonal = [[0, 4, 8], [2, 4, 6]]
+
 	var win_sum = 0
 	var block_sum = 0
-	
+
 	if Board.round_state == Board.TicTacToeRoundState.PLAYER_1_PICKING:
 		win_sum = 2
 		block_sum = 20
 	else:
 		win_sum = 20
 		block_sum = 2
-	
+
 	for row_sequence_list in [row_horizontal, row_vertical, row_diagonal]:
 		for sequence in row_sequence_list:
-			var sequence_values = [Board.board[sequence[0]], Board.board[sequence[1]], Board.board[sequence[2]]]
-			var sequence_sum = Board.board[sequence[0]] + Board.board[sequence[1]] + Board.board[sequence[2]]
-			
+			var sequence_values = [
+				Board.board[sequence[0]], Board.board[sequence[1]], Board.board[sequence[2]]
+			]
+			var sequence_sum = (
+				Board.board[sequence[0]]
+				+ Board.board[sequence[1]]
+				+ Board.board[sequence[2]]
+			)
+
 			# if this move will make selected player win
 			if sequence_sum == win_sum:
 				var pos = sequence_values.find(0)
-				return (sequence[pos])
+				return sequence[pos]
 			# if this move will block a player win
 			elif sequence_sum == block_sum:
 				var pos = sequence_values.find(0)
-				return (sequence[pos])
-	
+				return sequence[pos]
+
 	# If there isn't a move that will win or block a win, select a random open square
 	var open_squares = []
 	for i in range(Board.board.size()):
 		if Board.board[i] == 0:
 			open_squares.append(i)
-			
+
 	return open_squares[randi() % open_squares.size()]
 
+
 func check_for_win(player: int):
-	var win_horizontal = [[0,1,2], [3,4,5], [6,7,8]]
-	var win_vertical = [[0, 3, 6], [1,4,7], [2,5,8]]
-	var win_diagonal = [[0,4,8], [2,4,6]]
+	var win_horizontal = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+	var win_vertical = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+	var win_diagonal = [[0, 4, 8], [2, 4, 6]]
 
 	var win_sum = -1
 	if player == 0:
@@ -140,22 +149,27 @@ func check_for_win(player: int):
 		win_sum = 30
 	for win_sequence_list in [win_horizontal, win_vertical, win_diagonal]:
 		for sequence in win_sequence_list:
-			var sequence_sum = Board.board[sequence[0]] + Board.board[sequence[1]] + Board.board[sequence[2]]
+			var sequence_sum = (
+				Board.board[sequence[0]]
+				+ Board.board[sequence[1]]
+				+ Board.board[sequence[2]]
+			)
 			if sequence_sum == win_sum:
 				return true
-	
+
 	return false
+
 
 func init_grid():
 	var buttons = $ButtonLayer/TicTacToeGrid.get_children()
 	var image_squares = $TextureLayer/TextureGridContainer.get_children()
-	
+
 	for i in range(Board.board.size()):
 		if Board.board[i] != 0:
 			buttons[i].disabled = true
 		else:
 			buttons[i].disabled = false
-	
+
 	for i in range(Board.board.size()):
 		if Board.board[i] == 1:
 			image_squares[i].texture = x_image
