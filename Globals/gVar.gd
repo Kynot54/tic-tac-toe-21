84133cde8a4bfb62497ab74dbcb40e61e89de5fc
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-04-25 00:46:31 -0700

    Update gVar.gd
    Remove unnecessary export keywords

--- Globals/gVar.gd
+++ Globals/gVar.gd
@@ -1,12 +1,12 @@
 extends Node
 
-export(int) var player_score = 0
-export(int) var dealer_score = 0
-export(bool) var player_hit = false
-export(bool) var player1_win = false
-export(bool) var player2_win = false
-export(bool) var end = false
-export(bool) var new_round = true
+var player_score = 0
+var dealer_score = 0
+var player_hit = false
+var player1_win = false
+var player2_win = false
+var end = false
+var new_round = true
 var previewing_deck = false
 
 
