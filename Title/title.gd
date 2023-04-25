df0ebf5246d291d218d788e59c00bbc728607153
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-04-24 23:53:13 -0700

    Fix logo bounce animation
    Set pivot center on window resize

--- Title/title.gd
+++ Title/title.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Control
 
 # app semver
 var version = "1.5.3"
@@ -27,3 +27,15 @@ func _on_StartButtonCard_pressed():
 func _on_OptionsButtonCard_pressed():
 	Music.play_button_click(Music.ButtonType.TITLE_BUTTON)
 	Transit.change_scene("res://Options/Options.tscn")
+
+
+func _on_MainMenu_resized():
+	var logo = $MarginContainer/MainMenuContainer/PanelContainer/Logo
+	var logo_container = $MarginContainer/MainMenuContainer/PanelContainer
+	var logo_container_center = logo_container.get_global_transform().get_origin()
+	
+	logo_container_center.x += logo_container.rect_size.x/2
+	logo_container_center.y += logo_container.rect_size.y/2
+	
+	logo.rect_pivot_offset.x = logo_container_center.x
+	logo.rect_pivot_offset.y = logo_container_center.y
