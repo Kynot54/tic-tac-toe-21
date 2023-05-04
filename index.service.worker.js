be9a734cf2c4045ec262fae8c73e78c4ed0a55be
ethanl21 <15167713+ethanl21@users.noreply.github.com>
2023-05-04 03:29:40 +0000

    Deploying to gh-pages from @ Kynot54/tic-tac-toe-21@7b7a1210110751509a5c55a4e13fe88190f51d41 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1683163866|28433817";
+const CACHE_VERSION = "1683170966|16477887";
 const CACHE_PREFIX = "21 Marks the Spo-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
