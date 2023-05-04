382426c9bb902e765abb111e42af5788ed6d99d9
Kynot54 <36772228+Kynot54@users.noreply.github.com>
2023-05-04 00:20:18 +0000

    Deploying to gh-pages from @ Kynot54/tic-tac-toe-21@dbe63d3be7f17b9f5bcd1f20c62383b711489fed 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1683158430|27214493";
+const CACHE_VERSION = "1683159605|28923593";
 const CACHE_PREFIX = "tic-tac-toe-21-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
